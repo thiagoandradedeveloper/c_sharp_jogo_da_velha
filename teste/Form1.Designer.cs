bb116c76cd80c0fd9b606bedaa555eb0d69b9803
thiagoandradedeveloper <thiagoha3@gmail.com>
2024-05-24 01:14:16 -0300

    Finalizado 3

--- teste/Form1.Designer.cs
+++ teste/Form1.Designer.cs
@@ -145,6 +145,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(260, 302);
             this.Controls.Add(this.reset);
             this.Controls.Add(this.button9);
@@ -156,7 +157,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
